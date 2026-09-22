fc6af9c90137e93c6b292a022939479023c953b0
ユーザーマン <hiso26237@gmail.com>
2026-09-22 22:44:18 +0330

    Update FEXCorePreset.java

--- app/src/main/java/com/winlator/star/fexcore/FEXCorePreset.java
+++ app/src/main/java/com/winlator/star/fexcore/FEXCorePreset.java
@@ -7,6 +7,9 @@ public class FEXCorePreset {
     public static final String COMPATIBILITY = "COMPATIBILITY";
     public static final String INTERMEDIATE = "INTERMEDIATE";
     public static final String PERFORMANCE = "PERFORMANCE";
+    public static final String PERFORMANCE_TSO = "PERFORMANCE_TSO";
+    public static final String PERFORMANCE_X87_OFF = "PERFORMANCE_X87_OFF";
+    public static final String MAXIMUM_PERFORMANCE = "MAXIMUM_PERFORMANCE";
     public static final String DENUVO = "DENUVO";
     public static final String CUSTOM = "CUSTOM";
     public final String id;
